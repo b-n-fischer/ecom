1df1159d13a56a860f4e5e332f918c05f8228091
Bernd Fischer <bernd.noa.fischer@lunchio.de>
2019-02-11 02:53:48 +0100

    entity prototype cart

---
+++ app/entities/Cart.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entities;
+
+class Cart {
+
+    private $items = [];
+
+    public function add(Product $orderItem) {
+        array_push($this->items, $orderItem);
+    }
+
+    public function addMany(array $orderItems) {
+        $this->items = array_merge($this->items, $orderItems);
+    }
+
+    public function getTotalNetAmount() {
+        return array_reduce($this->items, function($sum, $item){
+            return $sum += $item->getNetAmount();
+        }, 0);
+    }
+
+    public function getTotalGross() {
+        return array_reduce($this->items, function($sum, $item){
+            return $sum += $item->getGross();
+        }, 0);
+    }
+}
