5b7ea9961f7f3678da7af8fc17893e5f97d0c6ee
Bernd Fischer <bernd.noa.fischer@lunchio.de>
2019-02-11 02:53:42 +0100

    entity prototype product

---
+++ app/entities/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entities;
+
+class Product {
+
+    private $name;
+    private $price;
+    private $taxation;
+
+    public function __construct(string $name,float $price,float $taxation) {
+        $this->name = $name;
+        $this->price = $price;
+        $this->taxation = $taxation;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getTaxation() {
+        return $this->taxation;
+    }
+
+    public function getNetAmount() {
+        return $this->price;
+    }
+
+    public function getGross() {
+        return $this->price * (1 + ($this->taxation / 100));
+    }
+}
